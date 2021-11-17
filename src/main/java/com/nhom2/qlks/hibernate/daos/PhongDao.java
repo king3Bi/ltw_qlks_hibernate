a5387ed344a790e186d9543abe06ea4233463b7f
Nguyen36 <88275484+Nguyen36@users.noreply.github.com>
2021-11-17 16:05:58 +0700

    Update PhongDao.java

--- src/main/java/com/nhom2/qlks/hibernate/daos/PhongDao.java
+++ src/main/java/com/nhom2/qlks/hibernate/daos/PhongDao.java
@@ -8,6 +8,7 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 import com.nhom2.qlks.hibernate.HibernateUtils;
+import com.nhom2.qlks.hibernate.pojo.HoaDon;
 import com.nhom2.qlks.hibernate.pojo.Phong;
 
 public class PhongDao {
@@ -16,7 +17,7 @@ public class PhongDao {
 		
         
         if (checkTenPhong(phong.getTenPhong())) {
-        	return err_msg = "Ten phòng đã tồn tại";
+        	return err_msg = "Ten phong da ton tai";
         }
         Transaction transaction = null;
         Session session = HibernateUtils.getFactory().openSession();
@@ -45,6 +46,87 @@ public class PhongDao {
         	   session.close();
         }
         return err_msg;
+	}
+	
+	public String updatePhong(int id,String tenphong) {
+		String err_msg = "";
+		
+		Transaction transaction = null;
+        Session session = HibernateUtils.getFactory().openSession();
+        
+        try {
+            // start a transaction
+            transaction = session.beginTransaction();            
+            System.out.println("created transaction");
+            
+            Query query = session.createQuery("UPDATE Phong SET tenPhong=:tenphong"
+            		+ " WHERE idPhong=:id");
+			query.setParameter("tenphong", tenphong);
+			query.setParameter("id", id);
+			int result = query.executeUpdate();
+			
+            System.out.println("update phong");
+            
+            // commit transaction
+            transaction.commit();
+            System.out.println("commited transaction");
+            
+            err_msg = "successed";
+        } catch (Exception e) {
+            if (transaction != null) {
+            	System.out.println("roll back transaction");
+                transaction.rollback();
+                err_msg = "failed";
+            }
+            e.printStackTrace();
+        } finally {
+        	   session.close();
+        }
+        return err_msg;
+	}
+	
+	public String deletePhong(int id) {
+		String err_msg = "";
+		
+		Transaction transaction = null;
+        Session session = HibernateUtils.getFactory().openSession();
+        
+        try {
+            // start a transaction
+            transaction = session.beginTransaction();            
+            System.out.println("created transaction");
+            
+            Query query = session.createQuery("DELETE FROM Phong WHERE idPhong=:id");
+			query.setParameter("id", id);
+			int result = query.executeUpdate();
+			
+            System.out.println("delete phong");
+            
+            // commit transaction
+            transaction.commit();
+            System.out.println("commited transaction");
+            
+            err_msg = "successed";
+        } catch (Exception e) {
+            if (transaction != null) {
+            	System.out.println("roll back transaction");
+                transaction.rollback();
+                err_msg = "failed";
+            }
+            e.printStackTrace();
+        } finally {
+        	   session.close();
+        }
+        return err_msg;
+	}
+	
+	public List<Phong> getAllHoaDon() {
+		Session session = HibernateUtils.getFactory().openSession();
+		Query q = session.createQuery("FROM Phong");//HQL
+		
+		List<Phong> phongs = q.getResultList();
+		
+		return phongs;
 	}
 	
 	public Phong getPhongById(int id) {
